2b28186c7f8c4dc11cddee5e0d1eb8f70feb8b74
DroopCat <35201355+DroopCat@users.noreply.github.com>
2022-05-31 21:51:15 -0500

    cleanup networking

--- static/appdev/networking.js
+++ static/appdev/networking.js
@@ -3,30 +3,30 @@ var socket = new window.WebSocket("wss://" + window.location.host);
 socket.onmessage = function(event) {
   console.log("WebSocket message received:", event.data);
   let message = JSON.parse(event.data);
-  if (message.msgType == 'lobbyListUpdated') {
-    lobbyUpdated(message.players);
-  } else if (message.msgType == 'updateGameState') {
-    if (message.state == "starting") {
-      preGameStart(message.cooldown);
-    }
-  } else if (message.msgType == 'updateGameSettings') {
-    gameSettings = message.settings;
-  } else if (message.msgType == 'assignGunID') {
-    playerGameData.gunID = message.GunID;
-  } else if (message.msgType == 'updateWeaponDefinitions') {
-    weaponDefinitions = message.weapons;
-  } else if (message.msgType == 'playerListUpdated') {
-    playerList = message.players;
-  } else if (message.msgType == 'kill') {
-    enemyKilled();
+  switch (message.msgType) {
+    case 'lobbyListUpdated':
+      lobbyUpdated(message.players);
+      break;
+    case 'updateGameState':
+      if (message.state == "starting") {
+        preGameStart(message.cooldown);
+      }
+      break;
+    case 'updateGameSettings':
+      gameSettings = message.settings;
+      break;
+    case 'assignGunID':
+      playerGameData.gunID = message.GunID;
+    case 'updateWeaponDefinitions':
+      weaponDefinitions = message.weapons;
+    case 'playerListUpdated':
+      playerList = message.players;
+    case 'kill':
+      enemyKilled();
   }
 };
 
-let clientInformation = { "msgType":"init", "client":"player" }
+let initMsg = { "msgType":"init", "client":"player" }
 socket.onopen = () => {
-  socket.send(JSON.stringify(clientInformation));
+  socket.send(JSON.stringify(initMsg));
 };
-
-setTimeout(()=>{
-  //socket.send("Hello!");
-}, 500);
